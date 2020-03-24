23e41051394ded6f914ae8d54ddde9eca5edfafe
Marc Mozgawa <marc.mozgawa@rte-france.com>
2020-03-24 17:12:32 +0100

    added counter folder

---
+++ src/counter/Counter.cpp
@@ -0,0 +1,8 @@
+#include "Counter.h"
+
+using namespace Counter;
+
+
+int Counter::Count(){
+    return 1;
+}
---
+++ src/counter/Counter.h
@@ -0,0 +1,9 @@
+#ifndef COUNTER_H
+#define COUNTER_H
+
+namespace Counter{
+    int value = 0;
+    int Count();
+}
+
+#endif /* ifndef COUNTER_H */
